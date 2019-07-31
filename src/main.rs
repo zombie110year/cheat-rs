a0bcb8e52461c28fe5aa9fdfd705b2787a06ff8a
zombie110year <zombie110year@outlook.com>
2019-07-31 20:10:22 +0800

    :tada: :sparkles: parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(non_snake_case)]
+
+use cheat::sheet;
+use clap::{App, Arg, ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let args = getArgs();
+    let name = args.value_of("NAME").unwrap();
+    let s = sheet::Sheet::new(String::from(name));
+    s.read();
+}
+
+fn getArgs() -> ArgMatches<'static> {
+    let matches = App::new("Cheat Sheet Manager")
+        .version("0.1.0")
+        .author("Zombie110year <zombie110year@outlook.com>")
+        .about("works as what the name described.")
+        .arg(
+            Arg::with_name("NAME")
+                .help("which cheat sheet to display")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    return matches;
 }
